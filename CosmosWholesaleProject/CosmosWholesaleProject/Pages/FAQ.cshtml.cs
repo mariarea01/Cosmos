4623aac500a12a0d3f4bd4ed5322eac6f3c1ed94
oostwoudere <64978024+oostwoudere@users.noreply.github.com>
2021-04-07 00:13:29 -0400

    Made a FAQ page and added it to the nav bar

---
+++ CosmosWholesaleProject/CosmosWholesaleProject/Pages/FAQ.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CosmosWholesaleProject.Pages
+{
+    public class FAQModel : PageModel
+    {
+        private readonly ILogger<FAQModel> _logger;
+
+        public FAQModel(ILogger<FAQModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
